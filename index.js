e245a424427fca467d5284d0fe59d870a38cf9fd
Tuguldur Baigalmaa <philoniare@gmail.com>
2017-06-05 01:30:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+const autobots = [
+  { name: 'Optimus Prime', strength: 5, isTransformed: false, },
+  { name: 'Ironhide', strength: 3, isTransformed: false, },
+  { name: 'Bumblebee', strength: 2.5, isTransformed: false, },
+  { name: 'Ratchet', strength: 1.5, isTransformed: false, },
+];
+
+function map(collection, callback) {
+  const result = [];
+
+  for (let i = 0; i < collection.length; i++) {
+    const element = collection[i];
+    result.push(callback(element, i, collection));
+  }
+
+  return result;
+}
+
+const transformedAutobotsWithMap = autobots.map(function (autobot) {
+  return Object.assign({}, autobot, {
+    strength: autobot.strength * 2,
+    isTransformed: true,
+  });
+});
+
+console.log(transformedAutobotsWithMap);
